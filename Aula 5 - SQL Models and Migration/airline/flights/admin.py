a63254e3f08ce20786a4ac8a8363ee2acc7ee123
Ewerton Mattos <ewerton.sc@gmail.com>
2021-05-19 02:07:06 -0300

    Personalizando o admin

--- Aula 5 - SQL Models and Migration/airline/flights/admin.py
+++ Aula 5 - SQL Models and Migration/airline/flights/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
 from .models import Flight, Airport, Passenger
+
+# Edita como a lista de flights vai ser exibida no admin.
+# Mais detalhes das opções disponiveis, ver documentação do Django
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration") # determina quais colunas apareceção na grid que lista os voos
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",) # Habilita duas caixas de seleção util quando se tem many to many
+
 # Register your models here.
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
