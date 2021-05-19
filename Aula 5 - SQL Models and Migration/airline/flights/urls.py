daeecec2cdce1844fce20af5f5dcd0b0401db623
Ewerton Mattos <ewerton.sc@gmail.com>
2021-05-19 01:58:24 -0300

    Adicionando um Passenger a um Flight

--- Aula 5 - SQL Models and Migration/airline/flights/urls.py
+++ Aula 5 - SQL Models and Migration/airline/flights/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:flight_id>", views.flight, name="flight")
+    path("", views.index, name="index"), # mostra uma lista de voos
+    path("<int:flight_id>", views.flight, name="flight"), # permite ver os detalhes de detreminado voo pelo ID
+    path("<int:flight_id>/book", views.book, name="book") # permite associar um passageiro à um voo
 ]
--- Aula 5 - SQL Models and Migration/airline/flights/views.py
+++ Aula 5 - SQL Models and Migration/airline/flights/views.py
@@ -1,6 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-
-from .models import Flight
+from django.urls import reverse
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -8,9 +9,29 @@ def index(request):
         "flights": Flight.objects.all()
     })
 
+
 def flight(request, flight_id):
-    flight = Flight.objects.get(pk=flight_id) # pk é uam maneira generica de se referenciar o pk do banco 
+    # pk é uam maneira generica de se referenciar o pk do banco
+    flight = Flight.objects.get(pk=flight_id)
+    np = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
-        "flight" : flight,
-        "passengers": flight.passengers.all()
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": np
     })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+
+        # assume que o passanger vai ser um campo do FORM postado chamado "passenger"
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        
+        # adiciona o passageiro ao Voo
+        passenger.flights.add(flight)
+
+        # redireciona para a URL do Voo específico. A rota flight aceita um argumento então eu passo o Id do Voo.
+        #reverse pega o nome de uma view e retorna a URL a qual aquele nome pertence.
+        return HttpResponseRedirect(reverse("flight", args=[flight.id]))
