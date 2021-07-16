6f0087a34aba3029ca1119761aea679cfbb956ae
Ewerton Mattos <ewerton.sc@gmail.com>
2021-07-16 17:25:20 -0300

    Added documentation to the function

--- Projetos/Project 4 - Network/project4/static/JSUtils.js
+++ Projetos/Project 4 - Network/project4/static/JSUtils.js
@@ -1,3 +1,4 @@
+/// If theres a file uploaded in the input, renders it an img control (imgElemID)
 function PreviewUploadedImage(input, imgElemID) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
